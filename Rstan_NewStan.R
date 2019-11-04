68b59bbdf180e45a7e088e8c80379e0555050c6e
amanda <kunbu@mail.usf.edu>
2019-11-04 13:38:23 -0500

    mat plot

--- Rstan_NewStan.R
+++ Rstan_NewStan.R
@@ -28,22 +28,24 @@ sigma <- rnorm(450,7,2)
 mat <- matrix(runif(450,1,3),nrow=N, ncol=2)
 mu_mat <- matrix(runif(450,1,3),nrow=N,ncol=2)
 sigma_mat <- matrix(runif(450,0,2),nrow=2, ncol=2)
-A <- rnorm(450,10,3)
-B <- rnorm(450,10,3)
+A <- rnorm(N,10,3)
+B <- rnorm(N,10,3)
 delta_H <- rnorm(450,10,1.5)
 sigma_for_prior <- matrix(c(1,1,1,1),2,2)
-beta1<- exp(log(A) + B*x1_stan + delta_H*x2_stan)
+# beta1<- exp(log(A) + B*x1_stan + delta_H*x2_stan)
 # for(n in 1:N){
 #   for(k in 1:K){
 #     beta1[(n-1)*5+k] <- exp(log(A[n]) + B[n]*x1_stan[(n-1)*5+k] + delta_H[n]*x2_stan[(n-1)*5+k]) 
 #   }
 # }
 
-beta0 <- mat[,1]
-gamma <- mat[,2]
+# beta0 <- mat[,1]
+# gamma <- mat[,2]
 
 # Set up the initial value of outcome
 for(n in 1:N){
+  beta0[n] <- mat[n,1]
+  gamma[n] <- mat[n,2]
   for(k in 1:K){
     beta1[(n-1)*5+k] <- exp(log(A[n]) + B[n]*x1_stan[(n-1)*5+k] + delta_H[n]*x2_stan[(n-1)*5+k])
     mu[(n-1)*5+k] <- beta0[n] + beta1[(n-1)*5+k] * (t_ijk[(n-1)*5+k]^gamma[n])
@@ -62,19 +64,19 @@ stan_data <- list(N=N,
                   x2=x2_stan)
 
 # Load Stan file
-fileName <- "New_Stan.stan"
-stan_code <- readChar(fileName,file.info(fileName)$size)
+stan_code <- "New_Stan.stan"
+
 # cat(stan_code)
 
 # Run Stan
-runStan <- stan(model_code=stan_code,data=stan_data, 
-                chains = 3, iter = 35000, warmup = 1000, thin = 10, init_r = .1, init=500)
+runStan <- stan("New_Stan.stan",data=stan_data, 
+                chains = 3, iter = 8500, warmup = 1000, thin = 10, init_r = .1)
 print(runStan, pars=c("mat"))
 
 
 list_of_draws <- extract(runStan)
 print(names(list_of_draws))
-dim(list_of_draws$A)
+dim(list_of_draws$mat)
 
 
 
